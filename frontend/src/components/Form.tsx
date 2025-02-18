5184840212d7c3cb3ffc9c41993f6a3376db1f05
vishal tiwari <vikranta1931@gmail.com>
2025-02-18 23:38:03 +0530

    form for link

--- frontend/src/components/Form.tsx
+++ frontend/src/components/Form.tsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 const Form=()=>{
+    const [url,setUrl]=useState('');
+         const handleChange=(e)=>{
+            setUrl(e.target.value)
+            console.log(e.target.value)
+         }
+         
+         const handleSubmit=(e)=>{
+            e.preventDefault()
+            setUrl("")
+         }
+    return(
 <div>
 <h2 className="text-center font-semibold">paste your link here !</h2>
-<form>
-<input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-<button className="btn btn-success">Download</button>
+<form onSubmit={handleSubmit}>
+<input type="text" placeholder="paste your url" className="input input-bordered w-full max-w-xs" value={url} />
+<button className="btn btn-success" type="submit">Download</button>
 </form>
 </div>
+    )
 }
 
 
