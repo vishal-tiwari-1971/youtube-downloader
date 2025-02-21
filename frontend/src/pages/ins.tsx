cd4c299e76173f5a0820d5c0fb0d077dee071cd2
vishal tiwari <vikranta1931@gmail.com>
2025-02-21 21:26:35 +0530

    ins page

--- frontend/src/pages/ins.tsx
+++ frontend/src/pages/ins.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import Form from "../components/Form";
 
 const Ins = () => {  // Capitalize component name (e.g., 'Ins')
-
+      return(
     <div>
       <h3 className='text-black'>Insta</h3>
-      {/* </>  This will render the 'Form' component */}
+      <Form/>  
     </div>
- 
+      )
 };
 
 export default Ins;  // Export the 'Ins' component (capitalized)
